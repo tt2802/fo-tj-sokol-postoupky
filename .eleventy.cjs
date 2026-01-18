3bcd35c787c0db6469abd224d88f7f4b0066e9dd
Tomas Kubin <tomasdanielkubin@gmail.com>
2026-01-18 12:09:01 +0100

    Fix Eleventy config for CI (czDate filter)

--- .eleventy.cjs
+++ .eleventy.cjs
@@ -3,7 +3,6 @@ const { DateTime } = require("luxon");
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy({ "src/assets": "assets" });
   eleventyConfig.addPassthroughCopy({ "src/admin": "admin" });
-  eleventyConfig.addPassthroughCopy({ "src/admin/config.yml": "admin/config.yml" });
   eleventyConfig.addPassthroughCopy({ "src/robots.txt": "robots.txt" });
 
   eleventyConfig.addFilter("czDate", (value) => {
